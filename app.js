48bc926763ba6b22f9ea92e3f90066a6a0c56c56
Antti Alavilo <A2302144@edu.lapinamk.fi>
2024-01-15 14:30:24 +0200

    Initialize the Yelp Camp project
    added the basic CRUD

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const Campground = require("./models/campground");
+
+mongoose.connect("mongodb://localhost:27017/yelp-camp");
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method")); //pass in the query string you want to use, in this case "_method"
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/campgrounds", async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });  //pass through "campgrounds" so that we can render them
+});
+
+app.get("/campgrounds/new", (req, res) => {
+    res.render("campgrounds/new");
+});
+
+app.post("/campgrounds", async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save(); //saves the new campground to the database
+    res.redirect(`/campgrounds/${campground._id}`);  // redirects you to the newly created campground by using a string template literal
+});
+
+app.get("/campgrounds/:id", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/show", { campground });
+});
+
+app.get("/campgrounds/:id/edit", async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render("campgrounds/edit", { campground });
+})
+
+app.put("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }); 
+    // here I use the spread-operator (...) to create a shallow copy of req.body.campground)
+    // the copy has the same properties as the original but any changes I do to the copied object will have no impact on the original one.
+    res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.delete("/campgrounds/:id", async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id); 
+    res.redirect(`/campgrounds`);
+});
+
+app.listen(3000, () => {
+    console.log.apply("Serving on port 3000!");
+});
