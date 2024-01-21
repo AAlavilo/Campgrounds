b6edef80bba7e4caf1bca0a30816c800e646b5aa
Antti Alavilo <A2302144@edu.lapinamk.fi>
2024-01-21 13:59:21 +0200

    Added a wrapper function and a basic error template
    title.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
+const catchAsync = require("./utils/catchAsync");
+const ExpressError = require("./utils/ExpressError");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const Campground = require("./models/campground");
@@ -35,20 +37,17 @@ app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 });
 
-app.post("/campgrounds", async (req, res, next) => {
-    try {
-        const campground = new Campground(req.body.campground);
-        await campground.save(); //saves the new campground to the database
-        res.redirect(`/campgrounds/${campground._id}`);  // redirects you to the newly created campground by using a string template literal
-    } catch (e) {
-        next(e)
-    }
-});
+app.post("/campgrounds", catchAsync(async (req, res, next) => {
+    if(!req.body.campground) throw new ExpressError("Invalid Campground Data", 400);
+    const campground = new Campground(req.body.campground);
+    await campground.save(); //saves the new campground to the database
+    res.redirect(`/campgrounds/${campground._id}`);  // redirects you to the newly created campground by using a string template literal
+}));
 
-app.get("/campgrounds/:id", async (req, res) => {
+app.get("/campgrounds/:id", catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     res.render("campgrounds/show", { campground });
-});
+}));
 
 app.get("/campgrounds/:id/edit", async (req, res) => {
     const campground = await Campground.findById(req.params.id);
@@ -69,7 +68,16 @@ app.delete("/campgrounds/:id", async (req, res) => {
     res.redirect(`/campgrounds`);
 });
 
+app.all("*", (req, res, next) => {                      // "app.all" means that it will perform with every request. The "*" means every single path. But it will still only run 
+    next(new ExpressError("Page Not Found", 404))       // if nothing else has matched first and there wasn't a response from any of them.
+})                                                      // By using next() we can pass the error to the next middleware.
+
 app.use((err, req, res, next) => {
+    // Destructure from the error. In ExpressError-class we have the the message and statusCode as parameters in the constructor
+    // We also put in some default values for the statusCode and the message
+    const { statusCode = 500 } = err;
+    if(!err.message) err.message = "Something Went Wrong";
+    res.status(statusCode).render("error", { err });
     res.send("Oh boy, Something went wrong")
 })
 
